f9e9285c86dd308856a968b063752afce4e87505
gregoryosler <oslerg21@greenhill.org>
2020-06-10 09:35:01 -0500

    GangstaName

---
+++ src/com/company/GangstaName.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class GangstaName {
+    public static void main(String[] args){
+    String Gdid= new String("G. diddy ");
+      String O= new String("OSLER ");
+          String fn= new String("Gregory-izzle");
+    System.out.print(Gdid);
+        System.out.print(O);
+        System.out.print(fn);
+}
+}
