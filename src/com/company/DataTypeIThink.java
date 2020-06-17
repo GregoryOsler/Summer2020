a645b8827765a9efaf18d03dae6d0f048fe37087
gregoryosler <oslerg21@greenhill.org>
2020-06-16 20:40:43 -0500

    created the data type program

--- src/com/company/DataTypeIThink.java
+++ src/com/company/DataTypeIThink.java
@@ -1,14 +1,45 @@
 package com.company;
+//Gregory Osler
+//06/16/2020
 
+//Pseudocode:
+//Declare my 2 integers, doubles, and strings
+//I then will set up my scanner
+//Then set up what the calculations will be
+//Then I will put in the code for the person to type things into the text box
+
+//I wrote my pseudocode after creating the code because I forgot to do it before (Sorry)
+
+
+import java.util.Scanner;
 public class DataTypeIThink {
     public static void main(String[] args) {
-        double dub1 = 5.45;
-        double dub2 = 6.99;
-        double dub3 = 8.43;
+        int int1;
+        double dub;
+        String str;
 
-        double calc1 = dub1 * dub2;
-        double calc2 = Math.pow(dub2, dub3);
-        double calc3 = dub1 + dub3;
-        System.out.printf(" This is my calculation \n %10.3f \n %16.3f \n %10.3f", calc1, calc2, calc3);
+        int i = 6;
+        double d = 5.45;
+        String s= "Greenhill";
+
+        Scanner console= new Scanner(System.in);
+        System.out.println("Type your integer: ");
+        int1= console.nextInt();
+        System.out.println("Enter your number with a decimal: ");
+        dub= console.nextDouble();
+        console= new Scanner(System.in);
+        System.out.println("Enter your phrase:");
+        str= console.nextLine();
+
+        int calc1= int1+ i;
+        double calc2= dub+ d;
+        String calc3= (str+ " "+ s);
+
+        System.out.printf("Integer= \n %10d", calc1);
+        System.out.println();
+        System.out.printf("Double= \n%10.1f", calc2);
+        System.out.println();
+        System.out.println("String= \n"+ calc3);
+
     }
 }
